e7db2232f9210d19603ad41d02e43c13ffdc9823
Aviv Zino <aviv@Avivs-MacBook-Pro.local>
2024-04-15 13:55:32 +0300

    changed configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/testvite",
+  base: "/flow-shop",
   test: {
     globals: true,
     environment: "jsdom",
